976aaa06b3fe90ade1b4417d10018794061f99b8
Daniel1835 <113837426+Daniel1835@users.noreply.github.com>
2023-03-11 03:26:53 +0800

    Add files via upload

---
+++ F55121075_Daniel Irwansyah_B_Tugas 4 PCD/Code/max_filter.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('Gambar asli/gambarasli median,min,max.jpg')
+
+max_filter = cv2.dilate(img, np.ones((3,3), np.uint8))
+
+cv2.imshow('Gambar Asli', img)
+cv2.imshow('Hasil Max Filter', max_filter)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
